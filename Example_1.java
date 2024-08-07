b76cec4d84bb0ad5f64f71abe1b2e3af0c397f06
Sourabh Mane <153292762+sourabtech@users.noreply.github.com>
2024-08-07 22:50:07 +0530

    Add files via upload

---
+++ Example_1.java
@@ -0,0 +1,54 @@
+package Chapter_1_Variables;
+
+public class Example_1 {
+
+	       /*Variables
+	
+			String 
+			int 
+			String 
+			
+			*/
+	
+	/* Primitive type:
+	   
+	   -length is fix 
+	 		
+	 		byte-1
+	 		short-4
+	 		int-4
+	 		long-8
+	 		float-4  decimal
+	 		double-8
+	 		char-2
+	 		boolean-1  true/false
+	 
+	 */
+	/*
+	     non-primitive types:
+	    
+	     -reference type and fix size no length
+	     
+	     String
+	     
+	     
+	 */
+	public static void main(String args[]) {
+		//premitive types
+		byte age=30;
+		int ph=1234567890;
+		long ph1=12345678901L;
+		float pi=3.14f;
+		char letter='@';
+		boolean isAdult=false;
+		
+		//non primetive types
+		String name="sam";
+		System.out.println(name.length());
+		
+		
+		
+		
+	}
+	
+}
