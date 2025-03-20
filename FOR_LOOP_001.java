a26f4651695c824f4f01951b9753cca7ed9e8a80
Sourabh Mane <153292762+sourabtech@users.noreply.github.com>
2025-03-20 21:11:50 +0530

    Add files via upload

---
+++ FOR_LOOP_001.java
@@ -0,0 +1,14 @@
+package Chapter_8_Conditional_Statement;
+
+public class FOR_LOOP_001 {
+
+	public static void main(String[] args) {
+
+		int sum=0;
+		for(int i=1;i<=10;i++) {
+		sum=sum+i;
+			System.out.println("The sume is print "  + sum);
+		}
+	}
+
+}
