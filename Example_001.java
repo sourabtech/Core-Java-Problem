1d20ce9f654f1171d033c6bcfbf858f0439e9c5f
Sourabh Mane <153292762+sourabtech@users.noreply.github.com>
2025-04-22 18:26:34 +0530

    Add files via upload

---
+++ Example_001.java
@@ -0,0 +1,19 @@
+package chapter_opps_3_Inheritance;
+
+  class Example_002 {
+	 float a=5;
+  }
+	class suggestion extends Example_001{
+		int b=6;
+		
+		
+	}
+	public class Example_001{
+		
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			suggestion sg=new suggestion();
+			System.out.println("my value is example_002 is:" + sg.b);
+	}
+
+}
