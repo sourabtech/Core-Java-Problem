78f575b89568cdc2a2b5d597b7d5a1c6d0b05355
Sourabh Mane <153292762+sourabtech@users.noreply.github.com>
2025-04-23 22:20:40 +0530

    Add files via upload

---
+++ Encapsulation.java
@@ -0,0 +1,14 @@
+package chapter_opps_5_Encapsulation;
+/*
+ 	Encapsulation in Java is a process of wrapping code and data together into a single unit. 
+ */
+
+	
+public class Encapsulation {
+private String student;
+	public static void main(String[] args) {
+	
+
+	}
+
+}
