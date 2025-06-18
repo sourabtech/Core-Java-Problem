ce2c520d566eac2d00036e77702f7c4a5190b89b
Sourabh Mane <153292762+sourabtech@users.noreply.github.com>
2025-06-18 10:45:57 +0530

    Add files via upload

---
+++ Example_3.java
@@ -0,0 +1,25 @@
+package Chapter_3_Arrays;
+
+public class Example_3 {
+
+	public static void main(String[] args) {
+
+		int age=23;
+		int hight=169;
+		int name=5;
+		
+		int[] info=new int[3];
+		
+		info[0]=23;
+		info[1]=5;
+		info[2]=169;
+		
+		System.out.println("print the total " + info[0] );
+		
+		System.out.println(info.length);
+		
+		
+
+	}
+
+}
